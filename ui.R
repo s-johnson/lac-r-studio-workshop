270c3408020e875cdcb54d15b43fff0e831e2ee5
s-johnson <sjohnson3@ph.lacounty.gov>
2022-09-07 18:06:23 +0000

    Update connect url

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@
 library(shiny)
 
 fluidPage(
-    titlePanel("Adverse Events Query to OpenFDA", windowTitle = "Adverse Events Query to OpenFDA"),
+    titlePanel("Adverse Events Query to OpenFDA 2022-09-07", windowTitle = "Adverse Events Query to OpenFDA"),
     fluidRow(
         column(3,
             br(),
